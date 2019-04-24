240f9ace3b8bcfd6d69fbf908b8259c1fc481cd7
akshay.khanna <akshay.khanna@thoughtworks.com>
2019-04-24 07:42:09 +0530

    Implement factory pattern

---
+++ FactoryPattern.go
@@ -0,0 +1,75 @@
+package main
+
+import "fmt"
+
+const (
+	TWO_WHEELER  = "Two wheeler vehicle"
+	FOUR_WHEELER = "Four wheeler vehicle"
+)
+
+type vehicle interface {
+	printVehicle()
+}
+
+type fourWheeler struct {
+}
+
+func (fw fourWheeler) printVehicle() {
+	fmt.Println("I am four wheeeler")
+}
+
+type twoWheeler struct {
+}
+
+func (tw twoWheeler) printVehicle() {
+	fmt.Println("I am two wheeler")
+}
+
+// creating class is optional, instead can use factory methods
+// Either this
+type vehicleFactory struct {
+}
+
+func (vf vehicleFactory) NewVehicle(typeOfVehicle string) vehicle {
+	if typeOfVehicle == TWO_WHEELER {
+		return &twoWheeler{}
+	} else if typeOfVehicle == FOUR_WHEELER {
+		return &fourWheeler{}
+	} else {
+		return nil
+	}
+}
+
+// or this, i.e. factory method
+func CreateVeichle(typeOfVehicle string) vehicle {
+	if typeOfVehicle == TWO_WHEELER {
+		return &twoWheeler{}
+	} else if typeOfVehicle == FOUR_WHEELER {
+		return &fourWheeler{}
+	} else {
+		return nil
+	}
+}
+
+func main() {
+	// EITHER THIS
+	// creating object via class factory
+	ourFactory := vehicleFactory{};
+
+	// create a four wheeler vehicle using factory CLASS
+	fourWheelerVehicle := ourFactory.NewVehicle(FOUR_WHEELER);
+	fourWheelerVehicle.printVehicle()
+
+	// create a two wheeler vehicle using factory CLASS
+	twoWheelerVehicle := ourFactory.NewVehicle(TWO_WHEELER);
+	twoWheelerVehicle.printVehicle()
+
+	// OR THIS
+	// create a four wheeler vehicle using factory METHOD
+	fourWheelerVehicle = CreateVeichle(FOUR_WHEELER);
+	fourWheelerVehicle.printVehicle()
+
+	// create a two wheeler vehicle using factory METHOD
+	twoWheelerVehicle = CreateVeichle(TWO_WHEELER);
+	twoWheelerVehicle.printVehicle()
+}
